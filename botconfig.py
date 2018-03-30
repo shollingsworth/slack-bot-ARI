0a06cbfdc1bf47d13ece11ce48ec71423683b209
Steven Hollingsworth <shollingsworth@barracuda.com>
2018-03-29 23:06:13 -0700

    Removed the get-slack-urls function for simplicity

--- botconfig.py
+++ botconfig.py
@@ -6,19 +6,14 @@ import os
 DEBUG = True
 
 # Basic bot name / emoji
-bot_name = "Annoying Repeating Insult Bot"
-bot_emoji = ":robot_face:"
-
-# randomize these... or don't
-url_oauth = 'rHLNr2iBeCIoK'
-url_command = 'kFl0hkZ4hFBZffWP'
-url_event = 'n3eTuYXk2Hzhea'
+bot_name    = "Annoying Repeating Insult Bot"
+bot_emoji   = ":robot_face:"
 
 # Environment Variable Names
-env_auth_file = 'SLACK_BOT_AUTH_FILE'
-env_client_id = 'SLACK_CLIENT_ID'
-env_verification_token = 'SLACK_VERIFICATION_TOKEN'
-env_client_secret = 'SLACK_CLIENT_SECRET'
+env_auth_file           = 'SLACK_BOT_AUTH_FILE'
+env_client_id           = 'SLACK_CLIENT_ID'
+env_verification_token  = 'SLACK_VERIFICATION_TOKEN'
+env_client_secret       = 'SLACK_CLIENT_SECRET'
 
 
 check_bad_environments = [
